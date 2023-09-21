b58e91cd24f8adcdb07a713dcf1b9f317f8d9940
jiangzhe <1024741733@qq.com>
2023-09-21 19:01:26 +0800

    init

--- electron/main/main.js
+++ electron/main/main.js
@@ -6,16 +6,14 @@ const indexHtml = join(process.env.DIST, 'dist/index.html')
 const createWindow = () => {
     const win = new BrowserWindow({
         minWidth: 1200,
-        width: 1200,
-        height: 800,
         minHeight: 800,
     })
-
     if (app.isPackaged) {
         win.loadFile(indexHtml)
     } else {
         win.loadURL('http://127.0.0.1:5173/')
     }
+    win.maximize()
 }
 
 app.whenReady().then(() => {
