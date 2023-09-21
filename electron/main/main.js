6ada8605b4761c92bb477c9ef54dc15ff5685259
jiangzhe <1024741733@qq.com>
2023-09-21 17:42:11 +0800

    init

--- electron/main/main.js
+++ electron/main/main.js
@@ -1,19 +1,28 @@
 const {app, BrowserWindow} = require('electron')
+const {join} = require("path");
+process.env.DIST = join(__dirname, '../../')
+const indexHtml = join(process.env.DIST, 'dist/index.html')
+
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600
     })
 
-    win.loadURL('http://127.0.0.1:5173/')
-    // win.loadFile('index.html')
+    if (app.isPackaged) {
+        win.loadFile(indexHtml)
+    } else {
+        win.loadURL('http://127.0.0.1:5173/')
+    }
 }
+
 app.whenReady().then(() => {
     createWindow()
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 })
+
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
