d89bd465569aee07e144147941a51be8cb970701
jiangzhe <1024741733@qq.com>
2023-09-23 18:01:03 +0800

    init

--- electron/main/main.js
+++ electron/main/main.js
@@ -4,19 +4,54 @@ process.env.DIST = join(__dirname, '../../')
 const indexHtml = join(process.env.DIST, 'dist/index.html')
 const windowStateKeeper = require('electron-window-state')
 
+const isMac = process.platform === 'darwin'
+
+const template = [// { role: 'appMenu' }
+    ...(isMac ? [{
+        label: app.name,
+        submenu: [{role: 'about'}, {type: 'separator'}, {role: 'services'}, {type: 'separator'}, {role: 'hide'}, {role: 'hideOthers'}, {role: 'unhide'}, {type: 'separator'}, {role: 'quit'}]
+    }] : []), // { role: 'fileMenu' }
+    {
+        label: 'File', submenu: [isMac ? {role: 'close'} : {role: 'quit'}]
+    }, // { role: 'editMenu' }
+    {
+        label: 'Edit',
+        submenu: [{role: 'undo'}, {role: 'redo'}, {type: 'separator'}, {role: 'cut'}, {role: 'copy'}, {role: 'paste'}, ...(isMac ? [{role: 'pasteAndMatchStyle'}, {role: 'delete'}, {role: 'selectAll'}, {type: 'separator'}, {
+            label: 'Speech', submenu: [{role: 'startSpeaking'}, {role: 'stopSpeaking'}]
+        }] : [{role: 'delete'}, {type: 'separator'}, {role: 'selectAll'}])]
+    }, // { role: 'viewMenu' }
+    {
+        label: 'View',
+        submenu: [{role: 'reload'}, {role: 'forceReload'}, {role: 'toggleDevTools'}, {type: 'separator'}, {
+            role: 'resetZoom',
+            enable: false
+        }, {role: 'zoomIn', enable: false}, {role: 'zoomOut',enable: false}, {type: 'separator'}, {role: 'togglefullscreen'}]
+    }, // { role: 'windowMenu' }
+    {
+        label: 'Window',
+        submenu: [{role: 'minimize'}, {role: 'zoom'}, ...(isMac ? [{type: 'separator'}, {role: 'front'}, {type: 'separator'}, {role: 'window'}] : [{role: 'close'}])]
+    }, {role: 'help', submenu: [{
+            label: 'Learn More', click: async () => {
+                const {shell} = require('electron')
+                await shell.openExternal('https://electronjs.org')
+            }
+        }]
+    }]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 const path = require('path')
+
+
 const createWindow = () => {
     // Menu.setApplicationMenu(null) // null值取消顶部菜单栏
     let status = windowStateKeeper({
-        defaultHeight : 618,
-        defaultWidth: 1000,
-        maximize: true
+        defaultHeight: 618, defaultWidth: 1000, maximize: true
     })
     const win = new BrowserWindow({
         ...status, webPreferences: {
             preload: path.join(__dirname, 'preload.js')
-        }, frame:false,
-        minHeight: 618, minWidth: 1000
+        }, frame: false, titleBarStyle: 'hidden', minHeight: 618, minWidth: 1000
     })
 
     if (app.isPackaged) {
@@ -24,7 +59,6 @@ const createWindow = () => {
     } else {
         win.loadURL('http://127.0.0.1:5173/')
     }
-
     status.manage(win)
     return win
 }
@@ -41,13 +75,6 @@ app.whenReady().then(() => {
             win.webContents.openDevTools()
         })
     }
-
-
-    globalShortcut.register('CommandOrControl+I', () => {
-        let contents = win.webContents
-        contents.openDevTools()
-    })
-
 })
 
 app.on('window-all-closed', () => {
